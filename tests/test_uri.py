22e1843c91be0470198200ba83316654eb1c4b9f
goose-life <32566441+goose-life@users.noreply.github.com>
2020-03-31 18:55:27 +0200

    add new failing test for akn prefix to be used as default

--- tests/test_uri.py
+++ tests/test_uri.py
@@ -6,6 +6,7 @@ from cobalt.uri import FrbrUri
 class FrbrUriTestCase(TestCase):
     def test_bad_value(self):
         assert_raises(ValueError, FrbrUri.parse, "/badness")
+        assert_raises(ValueError, FrbrUri.parse, "/ukpga/2015/1")
 
     def test_simple(self):
         uri = FrbrUri.parse("/za/act/1980/01")
@@ -21,21 +22,6 @@ class FrbrUriTestCase(TestCase):
         assert_is_none(uri.prefix)
 
         assert_equal("/za/act/1980/01", uri.work_uri())
-
-    def test_simple_prefix(self):
-        uri = FrbrUri.parse("/akn/za/act/1980/01")
-        assert_equal(uri.prefix, "akn")
-        assert_equal(uri.country, "za")
-        assert_equal(uri.locality, None)
-        assert_equal(uri.doctype, "act")
-        assert_equal(uri.subtype, None)
-        assert_equal(uri.actor, None)
-        assert_equal(uri.date, "1980")
-        assert_equal(uri.number, "01")
-        assert_equal(uri.language, "eng")
-        assert_equal(uri.expression_date, None)
-
-        assert_equal("/akn/za/act/1980/01", uri.work_uri())
 
     def test_with_subtype(self):
         uri = FrbrUri.parse("/za/act/by-law/1980/01")
@@ -259,3 +245,32 @@ class FrbrUriTestCase(TestCase):
         uri.format = 'html'
 
         assert_equal("/za/act/1980/02/eng@2014-01-01/main.html", uri.manifestation_uri())
+
+    def test_simple_prefix(self):
+        # also recognises akn prefix
+        uri = FrbrUri.parse("/akn/za/act/1980/01")
+        assert_equal(uri.prefix, "akn")
+        assert_equal(uri.country, "za")
+        assert_equal(uri.locality, None)
+        assert_equal(uri.doctype, "act")
+        assert_equal(uri.subtype, None)
+        assert_equal(uri.actor, None)
+        assert_equal(uri.date, "1980")
+        assert_equal(uri.number, "01")
+        assert_equal(uri.language, "eng")
+        assert_equal(uri.expression_date, None)
+
+        assert_equal("/akn/za/act/1980/01", uri.work_uri())
+
+    def test_akn_prefix(self):
+        # uses 'akn' prefix as the default when producing one
+        uri = FrbrUri(
+            country='za',
+            locality='ec',
+            doctype='act',
+            subtype='by-law',
+            date='2020',
+            number='31',
+            actor=None
+        )
+        assert_equal(uri.prefix, "akn")
